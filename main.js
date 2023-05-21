709ee250ef2cb5d10ee884c88ee60f0b127dd7e6
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2023-05-20 23:42:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var toggle = document.querySelector('toggle');
-var close = document.querySelector('close');
-var verticalMenu = document.querySelector('verticalMenu');
+var toggle = document.querySelector('.toggle');
+var close = document.querySelector('.close');
+var verticalMenu = document.querySelector('.verticalMenu');
 
 toggle.addEventListener('click', () => {
-verticalMenu.classList.toggle('show'); 
+verticalMenu.classList.toggle('show');
+toggle.classList.toggle('hide');
+close.classList.toggle('show');  
 });
