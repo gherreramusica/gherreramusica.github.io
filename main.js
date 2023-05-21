8bac2bb2a667bc66d33165c56009fc44750c826a
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2023-05-20 19:50:44 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var toggle = document.querySelector('toggle');
+var close = document.querySelector('close');
+var verticalMenu = document.querySelector('verticalMenu');
+
+toggle.addEventListener('click', () => {
+verticalMenu.classList.toggle('show'); 
+});
