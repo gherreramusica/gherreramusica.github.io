e37c8dbbece850f572f868c9c29c4d68f0174726
gherreramusica <132178396+gherreramusica@users.noreply.github.com>
2023-05-31 16:46:05 -0500

    update

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const bio = document.getElementById('bio');
+
+bio.addEventListener('click', function bio(){
+    document.write('HOLA MUNDO');
+})
+
 var toggle = document.querySelector('.toggle');
 var close = document.querySelector('.close');
 var verticalNav = document.querySelector('.verticalNav');
@@ -14,40 +20,33 @@ close.addEventListener('click', () =>{
 })
 
 
+//PARALAX
 
  var titulo = document.querySelector('.titulo');
  var imagen = document.querySelector('.imagen');
  var nota = document.querySelector('.nota');
  var checkpoint = document.querySelector('.checkpoint');
-
-
+ var vinil = document.querySelector('.vinil');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-   titulo.style.marginTop = value * 1.1 + 'px';
-    nota.style.marginBottom = value * 1.6 + 'px';
-    // checkpoint.style.marginTop = value * -.9 + 'px';   
- });
+    titulo.style.marginTop = value * 1.1 + 'px';
+    nota.style.marginBottom = value * -.4 + 'px';
+    vinil.style.marginBottom = value * -.7 + '10px';
+ })
 
-player
+ //MUSIC PLAYER
 
 var atras = document.querySelector('.atras');
 var play = document.querySelector('.play');
 var stop = document.querySelector('.stop');
 var siguiente = document.querySelector('.siguiente');
 var audio = document.querySelector('.audio');
-
-
-
 play.addEventListener('click', () => {
     audio.play();
 });
-
 stop.addEventListener('click', () => {
     audio.pause();
-})
+});
 
-var cabezal = document.querySelector('.titulo');
-
-cabezal.textContent= 'Martin';
 
 
